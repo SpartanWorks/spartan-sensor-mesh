def74a83ced90c84070f49d27bd620cbbc714637
Kajetan Rzepecki <k@spartan.works>
2017-09-16 01:38:26 +0200

    Moved router to a separate container.

--- frontend/src/app/main.tsx
+++ frontend/src/app/main.tsx
@@ -1,47 +1,8 @@
 import * as preact from "preact";
-import { route, Router } from "preact-router";
-import { Config } from "./containers/config/config";
-import { Dashboard } from "./containers/dashboard/dashboard";
+import { Main } from "./containers/main/main";
 import * as styles from "./main.css";
 import { SensorService } from "./services/sensor";
 import { MainStore } from "./store/main";
-
-interface RouteProps {
-  path?: string;
-  default?: boolean;
-}
-
-interface RouteWithStore extends RouteProps {
-  store: MainStore;
-}
-
-const DashboardRoute = (p: RouteWithStore) => (
-  <Dashboard store={p.store}/>
-);
-
-const ConfigRoute = (p: RouteProps) => (
-  <Config/>
-);
-
-class NotFoundRoute extends preact.Component<RouteProps, {}> {
-  componentWillMount() {
-    route("/", true);
-  }
-
-  render() {
-    return (
-      <div />
-    );
-  }
-}
-
-const Main = (p: RouteWithStore) => (
-  <Router>
-    <DashboardRoute store={p.store} path="/"/>
-    <ConfigRoute path="/config"/>
-    <NotFoundRoute default/>
-  </Router>
-);
 
 export function onLoad() {
   const mainStore = new MainStore();
