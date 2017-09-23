57da4dc950e9d76c38a2d5f1d66636f7b8a22fd0
Kajetan Rzepecki <k@spartan.works>
2017-09-24 01:52:10 +0200

    Abstracted Gauge component away from Reading.

--- frontend/src/app/components/reading/reading.tsx
+++ frontend/src/app/components/reading/reading.tsx
@@ -1,14 +1,12 @@
 import * as preact from "preact";
 import * as styles from "./reading.css";
 
-interface Props {
+interface GaugeProps {
   color: string;
   progress: number;
-  isError: boolean;
-  children?: Array<preact.Component<any, any>>;
 }
 
-export const Reading = (props: Props) => {
+const Gauge = (props: GaugeProps) => {
   const moreLess = (props.progress > 180) ? styles.progressMore : styles.progressLess;
   const leftStyle = {
     "border-color": props.color,
@@ -22,16 +20,27 @@ export const Reading = (props: Props) => {
   };
 
   return (
-    <div className={styles.wrapper + " " + moreLess}>
-      <div className={styles.shadow}/>
-      <div className={styles.gauge}>
-        <div className={styles.left + " " + styles.progressBar} style={leftStyle}></div>
-        <div className={styles.right + " " + styles.progressBar} style={rightStyle}></div>
-      </div>
-      <div className={styles.label}>
-        {props.children}
-      </div>
-      <div title="Sensor is not responding." className={props.isError ? styles.error : styles.hidden}/>
+    <div className={moreLess + " " + styles.gauge}>
+      <div className={styles.left + " " + styles.progressBar} style={leftStyle}></div>
+      <div className={styles.right + " " + styles.progressBar} style={rightStyle}></div>
     </div>
   );
-};
+}
+
+interface Props {
+  color: string;
+  progress: number;
+  isError: boolean;
+  children?: Array<preact.Component<any, any>>;
+}
+
+export const Reading = (props: Props) => (
+  <div className={styles.wrapper}>
+    <div className={styles.shadow}/>
+    <Gauge color={props.color} progress={props.progress}/>
+    <div className={styles.label}>
+      {props.children}
+    </div>
+    <div title="Sensor is not responding." className={props.isError ? styles.error : styles.hidden}/>
+  </div>
+);
