4d5acf4977ef7465d1144981bf7cdcad414724e7
Kajetan Rzepecki <k@spartan.works>
2017-10-04 20:44:29 +0200

    Made label text scale with its length.

--- frontend/src/app/components/gauge/label.tsx
+++ frontend/src/app/components/gauge/label.tsx
@@ -8,13 +8,18 @@ interface LabelProps {
   tooltip: string;
 }
 
-export const Label = (props: LabelProps) => (
-  <div className={styles.labelWrapper}>
-    <span className={styles.dummy}>{props.name}</span>
-    <div title={props.tooltip} className={styles.valueWrapper}>
-      <span className={styles.value}>{props.value}</span>
-      <span className={styles.unit}>{props.unit}</span>
+export const Label = (props: LabelProps) => {
+  const transform = {
+    "transform": "scale(" + Math.min(1.0, 5 / (props.value + props.unit).length) + ")"
+  };
+  return (
+    <div className={styles.labelWrapper}>
+      <span className={styles.dummy}>{props.name}</span>
+      <div title={props.tooltip} className={styles.valueWrapper} style={transform}>
+        <span className={styles.value}>{props.value}</span>
+        <span className={styles.unit}>{props.unit}</span>
+      </div>
+      <span title={props.name} className={styles.name}>{props.name}</span>
     </div>
-    <span title={props.name} className={styles.name}>{props.name}</span>
-  </div>
-);
+  );
+};
