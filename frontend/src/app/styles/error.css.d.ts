b846aed59513a2b3ffd851f69591101a53cb9804
Kajetan Rzepecki <k@spartan.works>
2017-11-20 23:52:46 +0100

    Refactored the error styles.

--- frontend/src/app/styles/error.css.d.ts
+++ frontend/src/app/styles/error.css.d.ts
@@ -1,5 +1,4 @@
 export const error: string;
 export const grow: string;
-export const errorTriggered: string;
+export const triggered: string;
 export const grayOut: string;
-export const grayOutTriggered: string;
