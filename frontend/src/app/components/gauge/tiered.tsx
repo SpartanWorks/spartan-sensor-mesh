75a6859a31976e573d4f20202d04cc553ba9b274
Kajetan Rzepecki <k@spartan.works>
2022-10-26 19:44:01 +0200

    Refactored the PM gauge into a more generic, tiered gauge.

---
+++ frontend/src/app/components/gauge/tiered.tsx
@@ -0,0 +1,41 @@
+import * as preact from "preact";
+import { ColorGauge } from "./color";
+
+export interface Tier {
+  min: number;
+  max: number;
+  color: string;
+}
+
+function identifyTier(value: number, tiers: Tier[], defaultTier: Tier): Tier {
+  const needle = tiers.find((tier) => {
+    if (tier.min <= value && tier.max > value) { return true; }
+    else { return false; }
+  });
+  return needle || defaultTier;
+}
+
+interface TieredGaugeProps {
+  value: number;
+  variance?: number;
+  tiers: Tier[];
+  defaultTier: Tier;
+  errorTooltip: string;
+  isError: boolean;
+  children: any;
+}
+
+export const TieredGauge = (props: TieredGaugeProps) => {
+  const tier = identifyTier(props.value, props.tiers, props.defaultTier);
+  return (
+    <ColorGauge value={props.value}
+                variance={props.variance}
+                min={tier?.min}
+                max={tier?.max}
+                color={tier?.color}
+                isError={props.isError}
+                errorTooltip={props.errorTooltip}>
+      {props.children}
+    </ColorGauge>
+  );
+};
--- frontend/src/app/components/pm/pm.tsx
+++ frontend/src/app/components/pm/pm.tsx
@@ -2,7 +2,28 @@ import * as preact from "preact";
 import { SensorData } from "../../services/device";
 import * as styles from "../../styles/widget.css";
 import { Label } from "../gauge/label";
-import { PMGauge } from "../gauge/pm";
+import { Tier, TieredGauge } from "../gauge/tiered";
+
+export const PM_10 = "pm10";
+export const PM_25 = "pm2.5";
+
+const PM_10_TIERS: Tier[] = [
+  { min: 0, max: 54, color: "#00e400" },
+  { min: 54, max: 154, color: "#ff0" },
+  { min: 154, max: 254, color: "#ff7e00" },
+  { min: 254, max: 354, color: "#f00" },
+  { min: 354, max: 424, color: "#8f3f97" },
+];
+
+const PM_25_TIERS: Tier[] = [
+  { min: 0, max: 12, color: "#00e400" },
+  { min: 12, max: 35, color: "#ff0" },
+  { min: 35, max: 55, color: "#ff7e00" },
+  { min: 55, max: 150, color: "#f00" },
+  { min: 150, max: 199, color: "#8f3f97" },
+];
+
+const PM_DEFAULT_COLOR = "#7e0023";
 
 interface Props {
   data: SensorData;
@@ -13,18 +34,17 @@ interface Props {
 export const PM = (props: Props) => (
   <div className={styles.widgetWrapper}>
     <div className={styles.readingWrapper}>
-      <PMGauge value={props.data.reading.mean}
-               variance={props.data.reading.variance}
-               min={props.min}
-               max={props.max}
-               isError={props.data.status === "error"}
-               errorTooltip="Sensor is not responding."
-               pmType={props.data.type}>
+      <TieredGauge value={props.data.reading.mean}
+                   variance={props.data.reading.variance}
+                   tiers={props.data.type === PM_10 ? PM_10_TIERS : PM_25_TIERS}
+                   defaultTier={{ min: props.min, max: props.max, color: PM_DEFAULT_COLOR }}
+                   isError={props.data.status === "error"}
+                   errorTooltip="Sensor is not responding.">
         <Label name={props.data.name[0].toUpperCase() + props.data.name.substring(1)}
                value={Number(props.data.reading.mean.toFixed(1))}
                unit="μg/m³"
                tooltip={"Averaged from last " + props.data.reading.samples + " readings."}/>
-      </PMGauge>
+      </TieredGauge>
     </div>
   </div>
 );
