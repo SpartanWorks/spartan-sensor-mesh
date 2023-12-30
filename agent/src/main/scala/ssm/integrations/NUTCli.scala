e16cb0b29a7a089340c411ae97c76e2e3c3bd339
Kajetan Rzepecki <k@spartan.works>
2023-12-30 23:09:24 +0100

    Added NUT integration tests.

--- agent/src/main/scala/ssm/integrations/NUTCli.scala
+++ agent/src/main/scala/ssm/integrations/NUTCli.scala
@@ -1,6 +1,6 @@
 package ssm.integrations
 
-import scala.util.Try
+import scala.util.{Try, Failure}
 
 import cats.effect.*
 import cats.implicits.*
@@ -16,40 +16,42 @@ trait NUTCli:
 
 object NUTCli:
   case class NUTVariable(name: String, value: Double)
-
   case class BadVariable(message: String) extends Throwable(message)
-  case class CliError(message: String) extends Throwable(message)
 
   def apply(): NUTCli =
-    new NUTCliImpl()
+    new NUTCliImpl(new Shell())
 
-  private class NUTCliImpl() extends NUTCli:
+  private[integrations] class Shell():
+      def exec(cmd: String, args: String*): IO[String] =
+        ProcessBuilder(cmd, args.toList).spawn[IO].use { process =>
+          process.stdout.through(text.utf8.decode).compile.string
+        }
+
+  private[integrations] class NUTCliImpl(shell: Shell) extends NUTCli:
     private val log = getLogger
 
     private def parseValue(value: String): IO[Double] =
-      IO.fromTry(Try(value.toDouble))
+      IO.fromTry(Try(value.trim.toDouble).orElse(Failure(BadVariable(s"Could not parse variable value: $value"))))
 
     private def parseLine(line: String): IO[NUTVariable] =
        line.split(":").map(_.trim) match {
          case Array(name, value) => parseValue(value).map(NUTVariable(name, _))
-         case _ => IO.raiseError(BadVariable(s"Could not parse variable value: ${line}"))
+         case _ => IO.raiseError(BadVariable(s"Could not parse variable: ${line}"))
        }
 
     private def run(cmd: String, args: String*): IO[String] =
-      ProcessBuilder(cmd, args.toList).spawn[IO].use { process =>
-        process.stdout.through(text.utf8.decode).compile.string
-      }.flatTap { output =>
+      shell.exec(cmd, args*).flatTap { output =>
         log.debug(s"Command '$cmd $args' resulted in: '$output'")
       }
 
     def fetchAll(ups: String): IO[List[NUTVariable]] =
       for
         output <- run("upsc", ups)
-        variables <- output.split("\n").toList.map(parseLine).sequence
+        variables <- output.trim.split("\n").toList.map(parseLine).sequence
       yield variables
 
     def fetch(ups: String, variable: String): IO[Double] =
       for
         output <- run("upsc", ups, variable)
-        value <- parseValue(output)
+        value <- parseValue(output.trim)
       yield value
